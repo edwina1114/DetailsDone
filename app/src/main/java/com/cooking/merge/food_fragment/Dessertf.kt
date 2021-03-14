e95ff429ba9a7e35dd6469ae3a3af53cda8bd49d
edwina1114 <ta517152139@gmail.com>
2021-03-14 20:56:29 +0800

    #全部食材頁面更新成功 #待加入相機及搜尋功能

--- app/src/main/java/com/cooking/merge/food_fragment/Dessertf.kt
+++ app/src/main/java/com/cooking/merge/food_fragment/Dessertf.kt
@@ -16,8 +16,7 @@ import com.cooking.merge.adapters.OnFoodItemClickListener
 import com.cooking.merge.model.FooditemsModel
 import kotlinx.android.synthetic.main.fooditems_layout.view.*
 
-class Dessertf: Fragment(), OnFoodItemClickListener
- {
+class Dessertf : Fragment(), OnFoodItemClickListener {
     private var recyclerView: RecyclerView? = null
     lateinit var gridLayoutManager: GridLayoutManager
     lateinit var foodList: ArrayList<FooditemsModel>
@@ -36,15 +35,22 @@ class Dessertf: Fragment(), OnFoodItemClickListener
     )
 
     private val ingredients = arrayOf(
-        "檸檬 1斤", "蔬菜 適量\n" + "水果 適量\n" +
-        "堅果(可有可無) 1小匙\n" + "開水 適量", "地瓜 1顆", "水果 適量\n" + "蘋果汁/乳酸飲料 適量\n" + "短冰棒棍/水果叉 適量",
+        "檸檬 1斤",
+        "蔬菜 適量\n" + "水果 適量\n" +
+                "堅果(可有可無) 1小匙\n" + "開水 適量",
+        "地瓜 1顆",
+        "水果 適量\n" + "蘋果汁/乳酸飲料 適量\n" + "短冰棒棍/水果叉 適量",
         "<香蕉麵糊>\n" + "熟香蕉 2大根\n" + "蛋 1顆\n" + "蔗糖/紅糖 150克\n" + "無鹽奶油 110克\n" + "希臘式優格(無糖為佳) 60克\n" +
-        "香草精 1小匙\n" + "低筋麵粉 125克\n" + "小蘇打粉 1小匙\n" + "鹽 1小撮", "小番茄 300克\n" + "酸梅 10~15顆\n" + "熱開水 100ml\n" +
-        "蜂蜜 5大匙\n" + "新鮮檸檬汁 1顆","大蘋果 1顆\n" + "奶油 10克" + "低筋麵粉 20克" + "蛋 1顆" + "牛奶 90克" + "糖粉(裝飾用) 適量" + "薄荷葉(裝飾用) 兩片",
-        "無鹽奶油 70克\n" + "黑糖粉 55克\n" +  "香草精 1/2小匙\n" + "蛋黃 1顆\n" + "中筋麵粉 110克\n" + "蘇打粉 1/4小匙\n" +
-        "巧克力豆 50克\n" + "無調味夏威夷豆 50克", "冷凍起酥皮 3片\n" + "巧克力 50克", "無鹽奶油 60克\n" + "低筋麵粉 225克\n" + "全麥麵粉 25克\n" +
-        "泡打粉 8克\n" + "原味優格 70克\n" + "鮮奶 30克\n" + "雞蛋 1顆 \n" + "蛋黃 1顆\n" + "蔓越莓乾/葡萄乾(可略) 50克\n" + "高筋麵粉 適量"
-        )
+                "香草精 1小匙\n" + "低筋麵粉 125克\n" + "小蘇打粉 1小匙\n" + "鹽 1小撮",
+        "小番茄 300克\n" + "酸梅 10~15顆\n" + "熱開水 100ml\n" +
+                "蜂蜜 5大匙\n" + "新鮮檸檬汁 1顆",
+        "大蘋果 1顆\n" + "奶油 10克" + "低筋麵粉 20克" + "蛋 1顆" + "牛奶 90克" + "糖粉(裝飾用) 適量" + "薄荷葉(裝飾用) 兩片",
+        "無鹽奶油 70克\n" + "黑糖粉 55克\n" + "香草精 1/2小匙\n" + "蛋黃 1顆\n" + "中筋麵粉 110克\n" + "蘇打粉 1/4小匙\n" +
+                "巧克力豆 50克\n" + "無調味夏威夷豆 50克",
+        "冷凍起酥皮 3片\n" + "巧克力 50克",
+        "無鹽奶油 60克\n" + "低筋麵粉 225克\n" + "全麥麵粉 25克\n" +
+                "泡打粉 8克\n" + "原味優格 70克\n" + "鮮奶 30克\n" + "雞蛋 1顆 \n" + "蛋黃 1顆\n" + "蔓越莓乾/葡萄乾(可略) 50克\n" + "高筋麵粉 適量"
+    )
 
     private val sauses = arrayOf(
         "冰糖 150公克\n" + "高粱醋 1斤", "不須調味料", "黑芝麻 1匙\n" + "鹽巴 少許\n" + "蜂蜜 適量", "不須調味料",
@@ -53,18 +59,25 @@ class Dessertf: Fragment(), OnFoodItemClickListener
         "白砂糖 40克\n" + "鹽 4克 \n"
     )
 
-     private val links = arrayOf(
-         "https://icook.tw/recipes/341359", "https://icook.tw/recipes/313727",
-         "https://cookpad.com/tw/%E9%A3%9F%E8%AD%9C/2069981-%E9%87%91%E9%BB%83%E8%8A%9D%E9%BA%BB%E5%9C%B0%E7%93%9C%E9%9B%BB%E9%8D%8B%E6%96%99%E7%90%86%E5%B0%8F%E8%B3%87%E5%A5%B3%E5%81%A5%E5%BA%B7%E5%B0%8F%E9%BB%9E%E5%BF%83#",
-         "https://icook.tw/recipes/257904", "https://icook.tw/recipes/255342",
-         "https://icook.tw/recipes/252287", "https://icook.tw/recipes/249785", "https://icook.tw/recipes/247907",
-         "https://icook.tw/recipes/243939", "https://icook.tw/recipes/243358"
-     )
+    private val links = arrayOf(
+        "https://icook.tw/recipes/341359",
+        "https://icook.tw/recipes/313727",
+        "https://cookpad.com/tw/%E9%A3%9F%E8%AD%9C/2069981-%E9%87%91%E9%BB%83%E8%8A%9D%E9%BA%BB%E5%9C%B0%E7%93%9C%E9%9B%BB%E9%8D%8B%E6%96%99%E7%90%86%E5%B0%8F%E8%B3%87%E5%A5%B3%E5%81%A5%E5%BA%B7%E5%B0%8F%E9%BB%9E%E5%BF%83#",
+        "https://icook.tw/recipes/257904",
+        "https://icook.tw/recipes/255342",
+        "https://icook.tw/recipes/252287",
+        "https://icook.tw/recipes/249785",
+        "https://icook.tw/recipes/247907",
+        "https://icook.tw/recipes/243939",
+        "https://icook.tw/recipes/243358"
+    )
     ////////////////////////////////////////////////array////////////////////////////////////////////////
 
     //(第二發生)顯示dessert_layout的介面
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fooditems_layout, container, false)
     }
@@ -84,32 +97,31 @@ class Dessertf: Fragment(), OnFoodItemClickListener
 
         foodList = ArrayList()
         foodList = addfood()
-        foodiesAdapters = FooditemsAdapter(requireContext(), this, foodList )   //adapter按照位置擺放foodlist裡的所有物品
+        foodiesAdapters =
+            FooditemsAdapter(requireContext(), this, foodList)   //adapter按照位置擺放foodlist裡的所有物品
         recyclerView?.adapter = foodiesAdapters
 
     }
 
-     fun addfood(): ArrayList<FooditemsModel> {
-         //array的方法 (way1)
-         val addlist: ArrayList<FooditemsModel> = ArrayList()
-         for (i in titles.indices)
-         {
-             val model = FooditemsModel(images[i], titles[i], ingredients[i], sauses[i], links[i])
-             addlist.add(model)
-         }
-         return addlist
-     }
+    fun addfood(): ArrayList<FooditemsModel> {
+        //array的方法 (way1)
+        val addlist: ArrayList<FooditemsModel> = ArrayList()
+        for (i in titles.indices) {
+            val model = FooditemsModel(images[i], titles[i], ingredients[i], sauses[i], links[i])
+            addlist.add(model)
+        }
+        return addlist
+    }
 
-     override fun onItemClick(item: FooditemsModel, position: Int)
-     {
-         val intent = Intent(context, FoodDetailsf::class.java)
-         intent.putExtra("FOODIMAGE", item.iconsChar.toString())
-         intent.putExtra("FOODNAME", item.alphaChar)
-         intent.putExtra("FOODINGREDIENT", item.ingredient)
-         intent.putExtra("FOODSAUCE", item.sauce)
-         intent.putExtra("FOODLINK", item.link)
+    override fun onItemClick(item: FooditemsModel, position: Int) {
+        val intent = Intent(context, FoodDetailsf::class.java)
+        intent.putExtra("FOODIMAGE", item.iconsChar.toString())
+        intent.putExtra("FOODNAME", item.alphaChar)
+        intent.putExtra("FOODINGREDIENT", item.ingredient)
+        intent.putExtra("FOODSAUCE", item.sauce)
+        intent.putExtra("FOODLINK", item.link)
 
-         startActivity(intent)
-     }
+        startActivity(intent)
+    }
 
 }
